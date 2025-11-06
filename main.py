d5d77fe9167ad680e942601eca091ecef74abf1e
Anastasiia Pohonets <29karasik@gmail.com>
2025-11-06 10:24:15 +0200

    Solution

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 import init_django_orm  # noqa: F401
+import json
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as f:
+        players = json.load(f)
+
+    for nickname, player_data in players.items():
+
+        race_info = player_data["race"]
+        race, _ = Race.objects.get_or_create(
+            name=race_info["name"],
+            defaults={"description": race_info.get("description", "")}
+        )
+
+        skills = race_info.get("skills", [])
+        for skill in skills:
+            skill_obj, _ = Skill.objects.get_or_create(
+                name=skill["name"],
+                race=race,
+                defaults={"bonus": skill.get("bonus", "")})
+
+        guilds = player_data.get("guild")
+        if guilds:
+            guild_obj, _ = Guild.objects.get_or_create(
+                name=guilds["name"],
+                defaults={"description": guilds.get("description")})
+        else:
+            guild_obj = None
+
+        Player.objects.get_or_create(nickname=nickname,
+                                     defaults={"email": player_data["email"],
+                                               "bio": player_data["bio"],
+                                               "race": race,
+                                               "guild": guild_obj}
+                                     )
 
 
 if __name__ == "__main__":
--- settings.py
+++ settings.py
@@ -6,6 +6,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # SECURITY WARNING: Modify this secret key if using in production!
 SECRET_KEY = "6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa"
 
+DEBUG = True
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 DATABASES = {
